d4b7737751b732c4c2f544f38449699662f453a1
Y3rseY <steaualuptava@gmail.com>
2024-04-14 08:43:56 +0300

    ALMOST DONE

--- client/src/SendCall.jsx
+++ client/src/SendCall.jsx
@@ -1,23 +1,40 @@
-import React from "react";
-//import 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function Sendd({selectedDate,selectedChain,onPriceSelected,onAccuracySelected}){
+function Sendd({ selectedDate, selectedChain, onPriceSelected, onAccuracySelected }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const unixTimeStampCall =() =>{
-        
-       const value = parseInt(selectedDate.getTime(), 10);
-       //callul de api cu rezultatul CU SELECTEDCHAIN acum imi da doar timestampu 
-       onPriceSelected(value)
-       onAccuracySelected( value/100000);
-       console.log( value);
-       console.log( {selectedChain});
-       //console.log()
+  const unixTimeStampCall = () => {
+    const timestamp = Math.round(selectedDate.getTime()); // Convert Date object to Unix timestamp in seconds
+    console.log('Timestamp:', timestamp);
+    console.log('Selected Chain:', selectedChain);
+
+    fetchData(timestamp);
+  };
+
+  const fetchData = async (timestamp) => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get(`https://cfcc.chickenkiller.com:5000/predict?coin=${selectedChain.substring(0, selectedChain.indexOf('(')).trim().toLowerCase()}&time=${timestamp}`);
+      const data = response.data;
+      console.log('API Response:', data);
+      onAccuracySelected(data.accuracy);
+      onPriceSelected(data.predicted_price);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
     }
-    return(
-        <div>
-            <button onClick={unixTimeStampCall}>BAGA BEATUU</button>
-        </div>
-    )
+  };
+
+  return (
+    <div>
+      <button onClick={unixTimeStampCall}>Fetch Data</button>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+    </div>
+  );
 }
 
 export default Sendd;
